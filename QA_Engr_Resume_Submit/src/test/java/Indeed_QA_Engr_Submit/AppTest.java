d88d9bd46bb970bc9786efd7eb62601b189f48cd
yiwei <yiwei@yiwei-Aspire-A315-21>
2018-09-19 13:18:23 -0400

    changed the format to plugin in the @CucumberOptions(format=)

--- QA_Engr_Resume_Submit/src/test/java/Indeed_QA_Engr_Submit/AppTest.java
+++ QA_Engr_Resume_Submit/src/test/java/Indeed_QA_Engr_Submit/AppTest.java
@@ -6,7 +6,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(format = {"pretty", "html:target/Destination/cucumber-htmlreport","json:target/Destination/cucumber-report.json"},features="src/test/java/Indeed_QA_Engr_Submit/outline.feature", glue="Indeed_QA_Engr_Submit")
+@CucumberOptions(plugin = {"pretty", "html:target/Destination/cucumber-htmlreport","json:target/Destination/cucumber-report.json"},features="src/test/java/Indeed_QA_Engr_Submit/outline.feature", glue="Indeed_QA_Engr_Submit")
 
 
 public class AppTest { }
